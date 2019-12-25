3114ca38e795c6a9a793bf82f58fa584147b0f1d
冯军 <w-feng.jun@rootcloud.com>
2019-12-25 19:05:56 +0800

    第一次提交

--- src/views/modules/generator/goods.vue
+++ src/views/modules/generator/goods.vue
@@ -6,7 +6,7 @@
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
-        <el-button v-if="isAuth('generator:goods:save')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
+        <el-button v-if="isAuth('generator:goods:save') " type="primary" @click="addOrUpdateHandle()">新增</el-button>
         <el-button v-if="isAuth('generator:goods:delete')" type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
       </el-form-item>
     </el-form>
